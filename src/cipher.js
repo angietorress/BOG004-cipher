96b63809ff7ae4063aab82c8d1d85fd3f61f2a85
angietorress <ange.torress19@gmail.com>
2022-02-25 14:31:55 -0300

    mi primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,51 @@
 const cipher = {
-  // ...
-};
+  encode: function (offset, text) {
+    let resultadoCifrado = "";
+    console.log("el tamaño del texto es:", text.length);
 
+    text = text.toUpperCase();
+    for (let i = 0; i < text.length; i++) {
+      let nuevaPosicion = 0;
+      let nuevaLetra = "";
+      console.log("en la posición ", i, "la letra es: ", text[i]);
+
+      if (text[i].charCodeAt() >= 65 && text[i].charCodeAt() <= 90) { //utilizacion del if para aplicar charCodeAt
+        nuevaPosicion =
+          ((text[i].charCodeAt() - 65 + parseInt(offset)) % 26) + 65; //formula aplicada
+
+        nuevaLetra = String.fromCharCode(nuevaPosicion);
+        resultadoCifrado = resultadoCifrado + nuevaLetra;
+
+      }
+    }
+    return resultadoCifrado;
+  },
+
+  decode: function (offset, text) {
+    //funcion para descifrar
+     console.log("el valor que ingresa en offset2 es:", offset);
+    console.log("el valor que ingresa para text2 es: ", text);
+    let resultadoDescifrado = "";
+    console.log("el tamaño del texto es:", text.length);
+    text = text.toUpperCase();
+    for (let i = 0; i < text.length; i++) {
+
+      let nuevaPosicion = 0;
+      let nuevaLetra = "";
+      console.log("en la posición ", i, "la letra es: ", text[i]);
+      
+      if (text[i].charCodeAt() >= 65 && text[i].charCodeAt() <= 90) {
+        console.log("esta letra está entre 65y90(A-Z) : ", text[i]);
+        nuevaPosicion = ((text[i].charCodeAt() - 65 -  parseInt(offset)) % 26) + 65; //esto tengo quer , cambiar formula
+        console.log("la nueva posicion es: ", nuevaPosicion);
+        nuevaLetra = String.fromCharCode(nuevaPosicion);
+        resultadoDescifrado = resultadoDescifrado + nuevaLetra;
+        console.log("la nueva letra es", resultadoDescifrado);
+        
+      }
+    }
+    return resultadoDescifrado;
+  }
+
+}; 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+//Obteniendo elementos del DOM con getElementById
+const offset = document.getElementById("rango");
+const text = document.getElementById("input-original");
+const btnCifrar = document.getElementById("btn-cifrar");
+const textCifrado = document.getElementById("texto-cifrado"); 
+
+const offset2 = document.getElementById("rango2");
+const text2 = document.getElementById("input-original2");
+const btnDescifrar = document.getElementById("btn-descifrar");
+const textDescifrado = document.getElementById("textoDescifrado"); 
+//funcion
+//Eventos del dom (registra un evento a un objeto)
+btnCifrar.addEventListener("click", function () {   //al escuchar click se realiza la funcion encode
+  let resultadoEncode= cipher.encode(offset.value, text.value);  // value da un valor a offset y text
+textCifrado.value=resultadoEncode
+});
+
+   
+ 
+btnDescifrar.addEventListener("click", function () { //al escuchar click se realiza la funcion decode
+    let resultadoDecode= cipher.decode(offset2.value, text2.value); // dandole valor a offset y text
+    textDescifrado.value= resultadoDecode
+
+});
